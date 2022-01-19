e40d5eebfd3d6af16af2635aca416c59a5c048d7
iraizo <raizo@riseup.net>
2022-01-19 01:12:29 +0100

    proto git

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
-use std::{process::Command, fs::File, io::{BufReader, BufRead}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+    process::Command,
+};
 
 use modules::steamcmd::Steamcmd;
 
+use crate::modules::{git::Git, types::Manifest};
+
 pub mod modules {
+    pub mod git;
     pub mod steamcmd;
     pub mod types;
-    mod git;
 }
 
 fn main() {
-    let cmd = Steamcmd::new(258550,258551, "out/".to_string());
+    let cmd = Steamcmd::new(258550, 258551, "out/".to_string());
     println!("[*] Fetching depot..");
-    let depot = cmd.fetch_depot().clone();
+    let mut depot = cmd.fetch_depot().clone();
 
     println!("[+] Done fetching depots.");
     for manifest in depot.manifests.clone() {
         let path = cmd.dir.clone() + &manifest.name;
         std::fs::create_dir_all(path).unwrap();
 
-        println!("[*] Downloading manifest {} ID: {}", &manifest.name, &manifest.id);
+        println!(
+            "[*] Downloading manifest {} ID: {}",
+            &manifest.name, &manifest.id
+        );
         cmd.download_manifest(&manifest);
     }
 
@@ -26,21 +35,51 @@ fn main() {
     println!("[*] Decompiling Assemblies..");
 
     let file = File::open("filter.txt").unwrap();
-    let filter: Vec<String> = BufReader::new(file).lines().collect::<Result<_, _>>().unwrap();
+    let filter: Vec<String> = BufReader::new(file)
+        .lines()
+        .collect::<Result<_, _>>()
+        .unwrap();
 
     for manifest in &depot.manifests {
         std::fs::create_dir_all(&String::from("pseudo/".to_owned() + &manifest.name)).unwrap();
         for file in &filter {
             let mut out = Command::new("/Users/raizo/.dotnet/tools/ilspycmd")
-            .args(["-o",
-            &String::from("pseudo/".to_owned() + &manifest.name),
-            &String::from("out/".to_owned() + &manifest.name + "/" + file)
-            ])
-            .spawn()
-            .expect("Failed");
+                .args([
+                    "-p",
+                    "-o",
+                    &String::from("pseudo/".to_owned() + &manifest.name),
+                    &String::from("out/".to_owned() + &manifest.name + "/" + file),
+                ])
+                .spawn()
+                .expect("Failed");
 
             out.wait().unwrap();
             println!("[*] Decompilation done of manifest {}", manifest.name);
         }
+    }
+
+    for manifest in &depot.manifests {
+        let mut repo = Git::new("repo".to_string(), manifest.name.clone());
+        repo.update();
+        println!("[*] Updated branch of {:?}", manifest.name);
+    }
+
+    /* TODO: dont hardcode this */
+    let main_repo = "public";
+
+    let mut repo = Git::new("repo".to_string(), main_repo.to_string());
+    repo.push();
+
+    let index = depot
+        .manifests
+        .iter()
+        .position(|x| x.name == main_repo)
+        .unwrap();
+    depot.manifests.remove(index);
+
+    for manifest in &depot.manifests {
+        let mut repo = Git::new("repo".to_string(), manifest.name.clone());
+        repo.push();
+        println!("[*] Pushed branch of {:?}", manifest.name);
     }
 }
--- src/modules/git.rs
+++ src/modules/git.rs
@@ -1,20 +1,82 @@
-use git2::{Repository, ResetType};
-
+use std::{
+    fs::{self, copy, read_dir},
+    io,
+    path::Path,
+    process::{Command, Stdio},
+};
+#[derive(Clone)]
 pub struct Git {
-    pub dir: String
+    pub dir: String,
+    pub manifest_name: String,
 }
 
 impl Git {
-    pub fn new(dir: String) -> Self {
-        return Self { dir }
+    pub fn new(dir: String, manifest_name: String) -> Self {
+        return Self { dir, manifest_name };
     }
 
-    pub fn initialize(&mut self) {
-        let repo = match Repository::init(self.dir.clone()) {
-            Ok(repo) => repo,
-            Err(e) => panic!("Failed to initialize git repo: {}", e),
-        };
+    pub fn update(&mut self) {
+        self.add();
+        self.commit();
+    }
 
+    pub fn push(&mut self) {
+        let mut out = Command::new("git")
+            .current_dir(self.dir.clone())
+            .args(["push", "origin", &self.manifest_name])
+            .stdin(Stdio::null())
+            .spawn()
+            .expect("Failed");
 
+        out.wait().unwrap();
+    }
+
+    pub fn add(&mut self) {
+        let mut out = Command::new("git")
+            .current_dir(self.dir.clone())
+            .args(["checkout", "-f", &self.manifest_name])
+            .stdin(Stdio::null())
+            .spawn()
+            .expect("Failed");
+
+        out.wait().unwrap();
+
+        copy(
+            &String::from("pseudo/".to_owned() + &self.to_owned().manifest_name),
+            &self.dir.clone(),
+        )
+        .unwrap();
+
+        /*    let dir = read_dir(&String::from(
+            "pseudo/".to_owned() + &self.to_owned().manifest_name,
+        ))
+        .unwrap();
+        for file in dir {
+            copy(
+                file.as_ref().unwrap().path(),
+                String::from(self.dir.clone() + "/" + file.unwrap().file_name().to_str().unwrap()),
+            )
+            .unwrap();
+        }*/
+
+        let mut out = Command::new("git")
+            .current_dir(self.dir.clone())
+            .stdin(Stdio::null())
+            .args(["add", "."])
+            .spawn()
+            .expect("Failed");
+
+        out.wait().unwrap();
+    }
+
+    pub fn commit(&mut self) {
+        let mut out = Command::new("git")
+            .current_dir(self.dir.clone())
+            .stdin(Stdio::null())
+            .args(["commit", "-m", "Update"])
+            .spawn()
+            .expect("Failed");
+
+        out.wait().unwrap();
     }
 }
